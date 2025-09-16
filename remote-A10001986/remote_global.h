80124de0294e2245e6176b49384937333506ecd4
A10001986 <76924199+realA10001986@users.noreply.github.com>
2025-09-16 21:17:46 +0200

    1.06
    - Add uploading mp3 files to SD card through Config Portal
    - WiFiManager patches to reduce size and memory fragmentation
    - WiFiManager fixes and additions

--- remote-A10001986/remote_global.h
+++ remote-A10001986/remote_global.h
@@ -15,8 +15,8 @@
  ***                          Version Strings                          ***
  *************************************************************************/
 
-#define REMOTE_VERSION "V1.05.1"
-#define REMOTE_VERSION_EXTRA "FEB132025"
+#define REMOTE_VERSION "V1.06"
+#define REMOTE_VERSION_EXTRA "SEP152025"
 
 //#define REMOTE_DBG              // debug output on Serial
 
